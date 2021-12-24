b333baec799228d14f185707dc7cae290b27cd39
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2021-12-24 16:55:13 +0100

    Add input-search, fix card, add cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30">Корзина</h2>
+
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div className="mb-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div className="mb-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between aligh-center p-40">
         <div className="d-flex align-center">
           <img src="/img/logo.svg" alt="Логотип" width={40} height={40} />
@@ -15,19 +43,23 @@ function App() {
             <span>1205 руб.</span>
           </li>
           <li>
-            <img
-              src="/img/user.svg"
-              alt="Личный профиль"
-              width={18}
-              height={18}
-            />
+            <img src="/img/user.svg" alt="Profile" width={18} height={18} />
           </li>
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кросовки</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все Кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input type="text" placeholder="Поиск..." />
+          </div>
+        </div>
         <div className="d-flex justify-between	">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="Unliked" />
+            </div>
             <img
               width={133}
               height={112}
